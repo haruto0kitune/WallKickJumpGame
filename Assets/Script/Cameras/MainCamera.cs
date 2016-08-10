5d488cf98fcbb54a7e3ccf604aa889eee33584f1
abe88j <yuutajb3@gmail.com>
2016-08-11 08:44:05 +0900

    Change motion of camera

--- Assets/Script/Cameras/MainCamera.cs
+++ Assets/Script/Cameras/MainCamera.cs
@@ -13,14 +13,9 @@ public class MainCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (player.transform.position.y > 0)
+        if (player.transform.position.y > transform.position.y)
         {
             transform.position = new Vector3(0, player.transform.position.y, transform.position.z);
-        }
-
-        if (player.transform.position.y < 0)
-        {
-            transform.position = new Vector3(0, 0, transform.position.z);
         }
 	}
 }
