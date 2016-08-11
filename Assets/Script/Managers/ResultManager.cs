28425fc695c34f2481ef0fe51a59490af63a4704
abe88j <yuutajb3@gmail.com>
2016-08-11 12:59:20 +0900

    implement adding up meter

---
+++ Assets/Script/Managers/ResultManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class ResultManager : MonoBehaviour
+{
+    ScoreManager scoreManager;
+    [SerializeField]
+    Text scoreText;
+    [SerializeField]
+    Text meterText;
+
+    void Awake()
+    {
+        scoreManager = ScoreManager.Instance;
+    }
+
+    void Start()
+    {
+        scoreManager.score.SubscribeToText(scoreText);
+        scoreManager.meter.Select(x => Mathf.Floor(x * 100) / 100).SubscribeToText(meterText);
+    }
+}
--- Assets/Script/Managers/ScoreManager.cs
+++ Assets/Script/Managers/ScoreManager.cs
@@ -6,17 +6,41 @@ using UniRx.Triggers;
 
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager Instance { get; private set; }
+
     [SerializeField]
-    Text text;
+    GameObject player;
+    [SerializeField]
+    Text scoreText;
+    [SerializeField]
+    Text meterText;
     public ReactiveProperty<int> score;
+    public ReactiveProperty<float> meter;
 
     void Awake()
     {
+        // Singleton
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // Initialize
         score = new ReactiveProperty<int>();
+        meter = new ReactiveProperty<float>();
     }
 
     void Start()
     {
-        score.SubscribeToText(text); 
+        this.UpdateAsObservable()
+            .Where(x => player.transform.position.y > meter.Value)
+            .Subscribe(_ => meter.Value = player.transform.position.y);
+
+        score.SubscribeToText(scoreText);
+        meter.Select(x => Mathf.Floor(x * 100) / 100).SubscribeToText(meterText);
     }
 }
