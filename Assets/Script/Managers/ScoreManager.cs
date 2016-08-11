88fd401dad349d925dc4ff8fc74d165e1c6401a1
abe88j <yuutajb3@gmail.com>
2016-08-11 09:10:39 +0900

    Implement getting items

---
+++ Assets/Script/Managers/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    Text text;
+    public ReactiveProperty<int> score;
+
+    void Awake()
+    {
+        score = new ReactiveProperty<int>();
+    }
+
+    void Start()
+    {
+        score.SubscribeToText(text); 
+    }
+}
---
+++ Assets/Script/Objects/Item.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    GameObject scoreManager;
+    ScoreManager scoreManagerComponent;
+
+    void Awake()
+    {
+        scoreManagerComponent = scoreManager.GetComponent<ScoreManager>();
+    }
+
+    void Start()
+    {
+        this.OnTriggerEnter2DAsObservable()
+            .Where(x => x.gameObject.tag == "Player")
+            .Subscribe(_ =>
+            {
+                scoreManagerComponent.score.Value++;
+                Destroy(this.gameObject);
+            }).AddTo(this.gameObject); 
+    }
+}
