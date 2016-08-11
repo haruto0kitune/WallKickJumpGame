507e167b263435da8e8ee02e487cad587714ca6d
abe88j <yuutajb3@gmail.com>
2016-08-11 12:20:28 +0900

    implement restart

---
+++ Assets/Script/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UniRx;
+using UniRx.Triggers;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    void Start()
+    {
+        player.OnBecameInvisibleAsObservable()
+            .ThrottleFirstFrame(60)
+            .Subscribe(_ =>
+            {
+                SceneManager.LoadScene(1);
+                SceneManager.UnloadScene(0);
+            });
+    }
+}
---
+++ Assets/Script/UI/RestartButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class RestartButton : MonoBehaviour
+{ 
+    void Start()
+    {
+
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
